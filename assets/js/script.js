57f79f260a9e3301dc8999ee582a9b096efe3ec7
Stephen Bevan <mrsteverox@gmail.com>
2024-10-17 13:54:49 +0000

    Add Event Listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,46 @@
-function rungame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
-function checkAnswer (){
+
+function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+
+function calculateCorrectAnswer() {
 
 }
-function incrementScore(){
+
+function incrementScore() {
 
 }
-function incrementWrongAnswer(){
+
+function incrementWrongAnswer() {
 
 }
-function displayAdditionQusestion(){
+
+function displayAdditionQuestion() {
 
 }
-function displayAdditionSubtractQuestion(){
+
+function displaySubtractQuestion() {
 
 }
-function displayMultiplyQusestion(){
+
+function displayMultiplyQuestion() {
     
 }
