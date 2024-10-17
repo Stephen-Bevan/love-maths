ac7170b1a10be11eef8da98cd9cef8e2a69c28ba
Stephen Bevan <mrsteverox@gmail.com>
2024-10-17 09:35:58 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function rungame(){
+
+}
+function checkAnswer (){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQusestion(){
+
+}
+function displayAdditionSubtractQuestion(){
+
+}
+function displayMultiplyQusestion(){
+    
+}
